fec8b394cb3b9ab45261d2ea49b23eeba468de07
Violeta <violet2021@foxmail.com>
2023-12-08 02:38:19 +0800

    1111

---
+++ DB_API.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+
+def DB_queryQuota(visitor_data:dict) -> int:
+    return 1
+
+def DB_queryTimeAndID(visitor_data:dict) -> str:
+    return None
+
+def DB_register(visitor_data:dict) -> None:
+    pass
